db56af75f6c1c82dbb53b9c2965a4522115e42fd
JayantiDeb2023 <137751194+JayantiDeb2023@users.noreply.github.com>
2023-06-28 15:42:50 +0530

    w3resource program4

---
+++ Program3.java
@@ -0,0 +1,14 @@
+package Project5;
+
+public class Program3 {
+ public enum weeks
+ {
+	 east,
+	 west,north,south
+ }
+ public static void main(String args[])
+ {
+	 weeks d1=weeks.east;
+	 System.out.println(d1);
+ }
+}
